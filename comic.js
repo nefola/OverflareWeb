362669b3d6f47d180ffb026cbd735910fb79ef92
Astral Abrams-Greenberg <nefolaC4@gmail.com>
2023-01-16 18:23:55 -0500

    1/16/23
    messed with page handing a lot, technically started work on the archival system

--- comic.js
+++ comic.js
@@ -1,6 +1,6 @@
-// code stolen from tracheotomy
+// code stolen from tracheotomy, by juneyblues
 // additonal code help from FlaringK, who helped write that code
-// this document contains code for: page numbers and page content. it may also contain the render code, we will see
+// this document contains code for: page numbers and page rendering
 function findGetParameter(parameterName) { //function used to write a parameter to append to the url, to give each comic page its own unique url
     let result = null,
         tmp = [];
@@ -17,9 +17,50 @@ let currentPage = Number(findGetParameter("pg"));
 nextPage = currentPage + 1;
 prevPage = currentPage - 1;
 
+// this function determins what the page selection text is linked to
+function setPageLinks(pg) {
+    let nextPage = document.querySelector(".nextpage")
+    let prevPage = document.querySelector(".prevpage")
+
+    nextPage.href = "?pg=" + (pg + 1)
+    prevPage.href = "?pg=" + (pg - 1)
+
+    if (pg - 1 < 0) { prevPage.style.display = "none" }
+    else { prevPage.style.display = "unset" }
+
+    if (pg + 1 >= pageList.length) { nextPage.style.display = "none" }
+    else { nextPage.style.display = "unset" }
+}
+
+// this code puts those links in
+
+setPageLinks(currentPage);
+document.querySelector(".nextpage").addEventListener("click", goToPage)
+document.querySelector(".prevpage").addEventListener("click", goToPage)
+
+// this code turns the page
+function goToPage(event) {
+    event.preventDefault()
+    history.pushState(null, '', this.href);
+
+    let linkedPage = parseInt(this.href.split("=")[1])
+    currentPage = linkedPage
+
+    // Set new links for page arrows
+    setPageLinks(linkedPage)
+    //clears the content
+    pageContent = "";
+    // Set new content
+    document.getElementById("pageContent").innerHTML = `${pageList[currentPage].content}`;
+    //get new theme
+    document.body.className = pageList[currentPage].theme
+    // Scroll Up 
+    window.scrollTo(0, 0);
+}
+
+
 
 
 // this code grabs page media and text for display
-
-// currerntly pagelist isnt defined
+// by using the pageContent id, it loads whatever html was in that page's
 document.getElementById("pageContent").innerHTML = `${pageList[currentPage].content}`;
--- pageData.js
+++ pageData.js
@@ -1,9 +1,22 @@
 var pageList = [
     {
         "title": "Example",
-        "pgNum": "0",
+        "pgNum": 0,
         "date":new Date("2023-1-15"),
         "content":`<img src=testImages/img4.jpg> `,
+    },
+    {
+        "title": "Example",
+        "pgNum": 1,
+        "date": new Date("2023-1-16"),
+        "content": `<img src=testImages/img3.jpg> `,
+},
+    {
+        "title": "Example",
+        "pgNum": 2,
+        "date": new Date("2023-1-16"),
+        "prevPage": 0,
+        "content": `<img src=testImages/img2.png> `,
     },
 ]
 
