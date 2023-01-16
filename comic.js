8ee9ef74f3e53dfddecb4f707fcff31d7bc0377a
Astral Abrams-Greenberg <nefolaC4@gmail.com>
2023-01-15 23:48:13 -0500

    1/15/23
    rendering works, now to fully implement page turning

--- comic.js
+++ comic.js
@@ -1,13 +1,25 @@
 // code stolen from tracheotomy
 // additonal code help from FlaringK, who helped write that code
 // this document contains code for: page numbers and page content. it may also contain the render code, we will see
-
+function findGetParameter(parameterName) { //function used to write a parameter to append to the url, to give each comic page its own unique url
+    let result = null,
+        tmp = [];
+    let items = location.search.substr(1).split("&");
+    for (let index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
 // this code handles page numbers
 let currentPage = Number(findGetParameter("pg"));
 
 nextPage = currentPage + 1;
 prevPage = currentPage - 1;
 
+
+
 // this code grabs page media and text for display
 
+// currerntly pagelist isnt defined
 document.getElementById("pageContent").innerHTML = `${pageList[currentPage].content}`;
--- pageData.js
+++ pageData.js
@@ -3,6 +3,11 @@ var pageList = [
         "title": "Example",
         "pgNum": "0",
         "date":new Date("2023-1-15"),
-        "content":`<img class=testClass src="$testImages/img4"> `,
+        "content":`<img src=testImages/img4.jpg> `,
     },
 ]
+
+/* export function myImageList() {
+    return pageList;
+}
+*/
